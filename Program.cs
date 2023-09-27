7519e12434469852a9cdd860e5fab4a080b6b9b4
TheBunnyMan123 <teguemcnair@gmail.com>
2023-09-27 15:25:48 -0500

    Add executable differentiation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Globalization;
+using Myrmec;
 
 class Program {
    static void Main() {
@@ -15,22 +16,43 @@ class Program {
         }
         Array.Sort<string>(tempFiles);
 
-        for (int i = 0; i < (files.Length); i++) {
-            if (File.Exists(files[i])) {
-                tempFinalFiles += "\x1b[32;49;1m     " + tempFiles[i];
+        for (int i = 0; i < (tempFiles.Length); i++) {
+            if (File.Exists(tempFiles[i])) {
+                // // create a sniffer instance.
+                // Sniffer sniffer = new Sniffer();
+
+                // // populate with mata data.
+                // // FileTypes.Common contains file types that we usually see.
+                // sniffer.Populate(FileTypes.Common);
+
+                // // get file head byte, may be 20 bytes enough.
+                // byte[] fileHead = ReadFileHead();
+
+                // // start match.
+                // List<string> results = sniffer.Match(fileHead);
+                // foreach (string type in results) {
+                //     Console.WriteLine(tempFiles[i] + ": " + type);
+                // };
+                // Console.WriteLine(tempFiles[i]);
+                // Console.WriteLine(Executable(tempFiles[i]));
+                if (Executable(tempFiles[i])) {
+                    tempFinalFiles += "\x1b[37;49;1m      " + tempFiles[i];
+                }else {
+                    tempFinalFiles += "\x1b[32;49;1m      " + tempFiles[i];
+                }
                 if (new StringInfo(tempFinalFiles).LengthInTextElements >= width-10-tempFiles[i].Length) {
                     finalFiles += "\n" + tempFinalFiles;
                     tempFinalFiles = "";
-                }else if (i == files.Length) {
+                }else if (i == tempFiles.Length) {
                     finalFiles += "\n" + tempFinalFiles;
                     tempFinalFiles = "";
                 }
             }else {
-                tempFinalFiles += "\x1b[34;49;1m     " + tempFiles[i];
+                tempFinalFiles += "\x1b[34;49;1m      " + tempFiles[i];
                 if (new StringInfo(tempFinalFiles).LengthInTextElements >= width-10-tempFiles[i].Length) {
                     finalFiles += "\n" + tempFinalFiles;
                     tempFinalFiles = "";
-                }else if (i == files.Length) {
+                }else if (i == tempFiles.Length) {
                     finalFiles += "\n" + tempFinalFiles;
                     tempFinalFiles = "";
                 }
@@ -38,4 +60,16 @@ class Program {
         }
         Console.WriteLine(finalFiles);
    }
+
+    static bool Executable(string filePath)
+    {
+        var firstBytes = new byte[13];
+        using(var fileStream = File.Open(filePath, FileMode.Open))
+        {
+            fileStream.Read(firstBytes, 0, 12);
+        }
+        // Console.WriteLine(System.Text.Encoding.UTF8.GetString(firstBytes));
+        var Bytes = System.Text.Encoding.UTF8.GetString(firstBytes);
+        return (Bytes.Contains("ELF") || Bytes.Contains("MZ") || Bytes.Contains("FEEDFACE") || Bytes.Contains("FEEDFACF") || Bytes.Contains("sh"));
+    }
 }
